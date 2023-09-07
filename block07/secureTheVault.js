3c9599bff19263c9415c26cf17bbd571dff69aa9
LuiguiGallardo <bfllg77@gmail.com>
2023-09-07 14:50:09 -0700

    Update to block07

---
+++ block07/secureTheVault.js
@@ -0,0 +1,15 @@
+// Pseudocode
+// 1. Create string
+// 2. Asign variables with operators
+// 3. Create the console.log() or alert()
+
+// User saying constant declaration
+const userSaying = "You have received this message because you have been chosen to open an important vault. Here is the secret combination."
+
+// Three variables declarations
+let var1 = 7 * 2
+let var2 = 7 - 1
+let var3 = 10 + 8
+
+console.log(`${userSaying}`)
+console.log(`${var1}${var2}${var3}`)
