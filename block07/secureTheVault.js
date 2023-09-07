cb91a2c730ac5c865306315d0d31bda6f66cbacb
LuiguiGallardo <bfllg77@gmail.com>
2023-09-07 15:17:09 -0700

    Update to block07

--- block07/secureTheVault.js
+++ block07/secureTheVault.js
@@ -4,12 +4,13 @@
 // 3. Create the console.log() or alert()
 
 // User saying constant declaration
-const userSaying = "You have received this message because you have been chosen to open an important vault. Here is the secret combination."
+const userSaying = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
 
 // Three variables declarations
 let var1 = 7 * 2
 let var2 = 7 - 1
 let var3 = 10 + 8
 
-console.log(`${userSaying}`)
-console.log(`${var1}${var2}${var3}`)
+// Console log and alert
+console.log(`${userSaying}${var1}${var2}${var3}`)
+alert(`${userSaying}${var1}${var2}${var3}`)
